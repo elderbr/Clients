b9b9f3b16685a30a18716528c55285c5948f5136
ElderBR <elderbarbosaromao@gmail.com>
2023-11-12 07:29:58 -0300

    Criando a entitade Client

---
+++ src/main/java/com/elderbr/Clients/entities/Client.java
@@ -0,0 +1,95 @@
+package com.elderbr.Clients.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String cpf;
+    private double income;
+    private LocalDate birdDate;
+    private int children;
+
+    public Client() {
+    }
+
+    public Client(long id, String name, String cpf, double income, LocalDate birdDate, int children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birdDate = birdDate;
+        this.children = children;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public LocalDate getBirdDate() {
+        return birdDate;
+    }
+
+    public void setBirdDate(LocalDate birdDate) {
+        this.birdDate = birdDate;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Client client = (Client) o;
+
+        return id == client.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+}
