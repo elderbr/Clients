e30fbe352db86e2e7656f8c35c4126e78c7caad1
ElderBR <elderbarbosaromao@gmail.com>
2023-11-12 09:15:55 -0300

    Criando o seeds

---
+++ src/main/java/com/elderbr/clients/controllers/ClientController.java
@@ -0,0 +1,2 @@
+package com.elderbr.clients.controllers;public class ClientController {
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_client (name, bird_date, cpf, children, income) VALUES ('Ana Souza', '1985-03-15', '12345678901', 2, 3500.0);
+INSERT INTO tb_client (name, bird_date, cpf, children, income) VALUES ('Marcos Oliveira', '1992-07-22', '98765432102', 1, 4800.0);
+INSERT INTO tb_client (name, bird_date, cpf, children, income) VALUES ('Renata Lima', '1980-12-10', '45678901203', 3, 5200.0);
+INSERT INTO tb_client (name, bird_date, cpf, children, income) VALUES ('Guilherme Santos', '1988-05-05', '78901234504', 0, 6000.0);
